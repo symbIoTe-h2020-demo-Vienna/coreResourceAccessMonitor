406fce1a5d703d6ea2f7e026d50787a8930fe419
vthglyk <vasgl@intracom-telecom.com>
2016-10-05 11:44:30 +0300

    Added unit_tests

--- src/test/java/eu/h2020/symbiote/CoreResourceAccessMonitorApplicationTests.java
+++ src/test/java/eu/h2020/symbiote/CoreResourceAccessMonitorApplicationTests.java
@@ -1,12 +1,7 @@
 package eu.h2020.symbiote;
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -15,32 +10,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.test.web.client.MockRestServiceServer;
 
 import java.nio.charset.Charset;
-import java.util.Map;
-import java.util.HashMap;
 import java.net.URL;
 
 import eu.h2020.symbiote.repository.RepositoryManager;
 import eu.h2020.symbiote.model.*;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers .method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
@@ -52,9 +38,6 @@ public class CoreResourceAccessMonitorApplicationTests {
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
-
-	@Autowired
-	private RestTemplate restTemplate;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
